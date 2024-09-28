04249ef388d1af9d1232184db6d7553d344fa101
Jacob Sabella <jacobsabella@outlook.com>
2024-09-28 08:12:49 -0500

    Bunch of puluim configs

---
+++ home-manager/user/jsabella/dev/pulumi-star/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Development environment for Pulumi STAR";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Python and necessary modules
+            gcc
+            python3
+            pulumi-bin
+            pulumiPackages.pulumi-language-python
+          ];
+
+          # Define the virtual environment path
+          VENV_DIR = "$HOME/Documents/projects/star-pulumi/venv";
+
+          shellHook = ''
+            # Change to the desired directory
+            cd ~/Documents/projects/star-pulumi
+
+            # Create and activate virtual environment if it doesn't exist
+            if [ ! -d "$VENV_DIR" ]; then
+              python3 -m venv $VENV_DIR
+            fi
+            source $VENV_DIR/bin/activate
+
+            # Export Pulumi passphrase
+            export PULUMI_CONFIG_PASSPHRASE="star_dev"
+
+            echo "Ready to deploy Pulumi STAR stuff"
+          '';
+        };
+      });
+}
+
